1c015663322c445818400874451fd001ac30c01f
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-04-06 15:13:55 +0300

    fix IDataContext

--- Data/Interfaces/IDataContext.cs
+++ Data/Interfaces/IDataContext.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace AirStar.Data.Interfaces
 {
     public interface IDataContext : IDisposable
     {
+        ChangeTracker ChangeTracker { get; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
     }
