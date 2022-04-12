d7e79d0b1948da1875c92df77fc41a6b91e2709e
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-04-12 15:19:43 +0300

    pause

---
+++ Models/Profile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirStar.ViewModels;
+using AutoMapper;
+
+namespace AirStar.Models
+{
+    public class Profile : AutoMapper.Profile
+    {
+        public Profile()
+        {
+            CreateMap<Aircraft, AircraftViewModel>();
+            CreateMap<Airport, AirportViewModel>();
+            CreateMap<Country, CountryViewModel>();
+            CreateMap<Flight, FlightViewModel>();
+            CreateMap<Passenger, PassengerViewModel>();
+            CreateMap<Price, PriceViewModel>();
+            CreateMap<Rate, RateViewModel>();
+            CreateMap<Reservation, ReservationViewModel>();
+            CreateMap<Route, RouteViewModel>();
+            CreateMap<User, RegistrationViewModel>();
+            CreateMap<User, LogInViewModel>();
+        }
+    }
+}
---
+++ ViewModels/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirStar.Models;
+
+namespace AirStar.ViewModels
+{
+    public class Profile : AutoMapper.Profile
+    {
+        public Profile()
+        {
+            CreateMap<AircraftViewModel, Aircraft>();
+            CreateMap<AirportViewModel, Airport>();
+            CreateMap<CountryViewModel, Country>();
+            CreateMap<FlightViewModel, Flight>();
+            CreateMap<PassengerViewModel, Passenger>();
+            CreateMap<PriceViewModel, Price>();
+            CreateMap<RateViewModel, Rate>();
+            CreateMap<ReservationViewModel, Reservation>();
+            CreateMap<RouteViewModel, Route>();
+            CreateMap<RegistrationViewModel, User>();
+            CreateMap<LogInViewModel, User>();
+        }
+    }
+}
