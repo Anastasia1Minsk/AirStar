b7ad53410a7b2748fad00d149d13b0defb8cb7d1
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-04-14 12:48:57 +0300

    fix validation

--- Validations/RegistrationValidator.cs
+++ Validations/RegistrationValidator.cs
@@ -17,7 +17,7 @@ namespace AirStar.Validations
             RuleFor(x => x.Password).NotEmpty().
                 Length(5,50)
                 .Matches(@"(?= (.*[0 - 9]))(?=.*[a - z])(?= (.*[A - Z])).{ 5,}")
-                .WithMessage("Please fill out the Password field");
+                .WithMessage("Please fill out the Password field. Password should contain numbers, upper case and lower case letters");
             RuleFor(x => x.ConfirmPassword).NotEmpty()
                 .Equal(x => x.Password)
                 .WithMessage("Please fill out the Confirm password field");
