e049509e2e6c9b82b26234116cc5a7b98d83a182
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-04-21 22:16:08 +0300

    Country catalog

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -11,15 +11,6 @@ namespace AirStar.Models
     {
         public Profile()
         {
-            /*CreateMap<Aircraft, AircraftViewModel>();
-            CreateMap<Airport, AirportViewModel>();
-            CreateMap<Country, CountryViewModel>();
-            CreateMap<Flight, FlightViewModel>();
-            CreateMap<Passenger, PassengerViewModel>();
-            CreateMap<Price, PriceViewModel>();
-            CreateMap<Rate, RateViewModel>();
-            CreateMap<Reservation, ReservationViewModel>();
-            CreateMap<Route, RouteViewModel>();*/
             CreateMap<User, RegistrationViewModel>();
             CreateMap<User, LogInViewModel>();
         }
---
+++ Validations/CountryValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirStar.Models;
+using AirStar.ViewModels;
+using FluentValidation;
+
+namespace AirStar.Validations
+{
+    public class CountryValidator : AbstractValidator<Country>
+    {
+        public CountryValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Please fill out the Name field")
+                .Length(2, 50)
+                .WithMessage("Lenght should be between 2 to 50")
+                .Matches(@"^[a-zA-Z -]*$")
+                .WithMessage("Name shouldn't contain special characters");
+        }
+    }
+}
