6a1e0108889307065b9aeb461f5b40f5c44ba33f
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2022-05-10 21:14:29 +0300

    registration design

--- Validations/RegistrationValidator.cs
+++ Validations/RegistrationValidator.cs
@@ -13,19 +13,19 @@ namespace AirStar.Validations
         {
             RuleFor(x => x.Email)
                 .NotEmpty()
-                .WithMessage("Please fill out the Password field")
+                .WithMessage("Please fill out the Email field")
                 .EmailAddress()
                 .WithMessage("Please fill out the Email field correctly");
             RuleFor(x => x.Password)
                 .NotEmpty()
                 .WithMessage("Please fill out the Password field")
                 .Length(5,50)
-                .WithMessage("Lenght should be between 5 to 50")
+                .WithMessage("Lenght should be between 5 to 50 symbols")
                 .Matches(@"(?=(.*[0-9]))(?=.*[a-z])(?=(.*[A-Z])).{5,}")
                 .WithMessage("Password should contain numbers, upper case and lower case letters");
             RuleFor(x => x.ConfirmPassword)
                 .NotEmpty()
-                .WithMessage("Please fill out the Password field")
+                .WithMessage("Please fill out the Confirm Password field")
                 .Equal(x => x.Password)
                 .WithMessage("Please fill out the Confirm password field correctly");
         }
