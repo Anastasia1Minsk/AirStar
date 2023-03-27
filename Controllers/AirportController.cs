9192e83e359d28f560a6c66997c7bcd3406cf67b
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2023-03-27 22:02:28 +0300

    Airport controller

---
+++ Controllers/AirportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirStar.Controllers
+{
+    public class AirportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
